aaa549c3308d2c763ea17982f191f50007571c57
andreacataluffi <andrea.cataluffi@e-lios.eu>
2025-08-20 11:24:11 +0200

    Minor edits and graphical reordering

--- client.js
+++ client.js
@@ -14,7 +14,7 @@ import ServicesModule from './services/ServicesModule';
 
 registerBpmnJSModdleExtension(spaceModdle);
 registerBpmnJSPlugin(ServicesModule); 
-// registerBpmnJSPlugin(TypedPaletteProvider);
+// registerBpmnJSPlugin(TypedPaletteProvider); For the moment i removed the palette on the left for custom task
 registerBpmnJSPlugin(TypedOverlay);
 registerBpmnJSPlugin(MovementContextPadProvider);
 registerBpmnJSPlugin(SpacePropertiesProvider); 
